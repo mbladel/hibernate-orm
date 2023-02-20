4d2c7c438dca97d35285216ab3503e0c9a5e6205
Marco Belladelli <marco@hibernate.org>
2023-02-20 14:57:19 +0100

    HHH-16097 Moved chapters envers (to hibernate-envers), events and finished domain (collections, inheritance, immutability and naming)

--- hibernate-core/src/test/java/org/hibernate/orm/test/mapping/identifier/SimplePartitionKeyTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/identifier/SimplePartitionKeyTest.java
@@ -4,7 +4,7 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.userguide.mapping.identifier;
+package org.hibernate.orm.test.mapping.identifier;
 
 import org.hibernate.annotations.PartitionKey;
 import org.hibernate.orm.test.jpa.BaseEntityManagerFunctionalTestCase;
