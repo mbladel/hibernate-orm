c78cfb04c24cb28573321ee7266fa1657a5c7331
Marco Belladelli <marco@hibernate.org>
2023-05-09 15:59:38 +0200

    HHH-16555 --wip-- better testing

--- hibernate-core/src/test/java/org/hibernate/orm/test/inheritance/JoinFetchInheritanceTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/inheritance/JoinFetchInheritanceTest.java
@@ -17,7 +17,6 @@ import org.hibernate.testing.orm.junit.SessionFactoryScope;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.DiscriminatorType;
@@ -30,11 +29,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.TypedQuery;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -45,45 +42,51 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SessionFactory
 @DomainModel( annotatedClasses = {
 		JoinFetchInheritanceTest.Animal.class,
-		JoinFetchInheritanceTest.Dog.class,
 		JoinFetchInheritanceTest.Cat.class,
 		JoinFetchInheritanceTest.CatEmbedded.class,
 		JoinFetchInheritanceTest.Kitten.class
 } )
 public class JoinFetchInheritanceTest {
+	private final static String CAT = "cat";
+	private final static String CAT_EMBEDDED = "cat_embedded";
+
 	@BeforeAll
 	public void setUp(SessionFactoryScope scope) {
 		scope.inTransaction( session -> {
 			final List<Animal> animals = new ArrayList<>();
-			animals.add( new Dog( 1L, "Dog" ) );
-			animals.add( new Cat( 2L, List.of( new Kitten( "Kitten" ) ) ) );
-			animals.add( new Cat( 3L, new ArrayList<>() ) );
-			animals.add( new CatEmbedded( 4L, new KittensEmbeddable( List.of( new Kitten( "EmbeddedKitten" ) ) ) ) );
-			animals.add( new CatEmbedded( 5L, new KittensEmbeddable( new ArrayList<>() ) ) );
+			final Kitten kitten1 = new Kitten( "kitten_1" );
+			session.persist( kitten1 );
+			animals.add( new Cat( 1L, List.of( kitten1 ), kitten1 ) );
+			animals.add( new Cat( 2L, new ArrayList<>(), kitten1 ) );
+			final Kitten kitten2 = new Kitten( "kitten_2" );
+			session.persist( kitten2 );
+			animals.add( new CatEmbedded( 3L, new KittensEmbeddable( List.of( kitten2 ), kitten2 ) ) );
+			animals.add( new CatEmbedded( 4L, new KittensEmbeddable( new ArrayList<>(), kitten2 ) ) );
 			animals.forEach( session::persist );
 		} );
 	}
 
 	@Test
 	public void testCollection(SessionFactoryScope scope) {
-		final List<Animal> animals = scope.fromTransaction( session -> {
-			final TypedQuery<Animal> query = session.createQuery(
-					"select animal from Animal animal left join fetch animal.kittens",
-					Animal.class
-			);
-			return query.getResultList();
-		} );
+		final List<Animal> animals = scope.fromTransaction( session -> session.createQuery(
+				"select animal from Animal animal left join fetch animal.kittens",
+				Animal.class
+		).getResultList() );
+		assertThat( animals ).hasSize( 4 );
 		for ( final Animal animal : animals ) {
-			if ( animal instanceof Cat ) {
-				final Cat cat = (Cat) animal;
-				final List<Kitten> kittens = cat.getKittens();
-				assertThat( Hibernate.isInitialized( kittens ) ).isTrue();
-				assertThat( kittens ).hasSizeBetween( 0, 1 );
+			switch ( animal.getType() ) {
+				case CAT:
+					final Cat cat = (Cat) animal;
+					final List<Kitten> kittens = cat.getKittens();
+					assertThat( Hibernate.isInitialized( kittens ) ).isTrue();
+					assertThat( kittens ).hasSizeBetween( 0, 1 );
+					break;
+				case CAT_EMBEDDED:
+					final CatEmbedded catEmbedded = (CatEmbedded) animal;
+					final List<Kitten> kittensEmbedded = catEmbedded.getKittensEmbeddable().getKittens();
+					assertThat( Hibernate.isInitialized( kittensEmbedded ) ).isFalse();
 			}
 		}
-
-		// todo marco : also test with Cat.embeddable.kittens to be extra sure
-		// todo marco : would be nice to have a solution which creates the correct nav. path from the join side
 	}
 
 	@Test
@@ -95,6 +98,21 @@ public class JoinFetchInheritanceTest {
 			);
 			return query.getResultList();
 		} );
+		assertThat( animals ).hasSize( 4 );
+		for ( final Animal animal : animals ) {
+			switch ( animal.getType() ) {
+				case CAT:
+					final Cat cat = (Cat) animal;
+					final Kitten kitten = cat.getSingleKitten();
+					assertThat( Hibernate.isInitialized( kitten ) ).isTrue();
+					assertThat( kitten.getName() ).isEqualTo( "kitten_1" );
+					break;
+				case CAT_EMBEDDED:
+					final CatEmbedded catEmbedded = (CatEmbedded) animal;
+					final Kitten kittenEmbedded = catEmbedded.getKittensEmbeddable().getSingleKitten();
+					assertThat( Hibernate.isInitialized( kittenEmbedded ) ).isFalse();
+			}
+		}
 	}
 
 	@Test
@@ -106,12 +124,19 @@ public class JoinFetchInheritanceTest {
 			);
 			return query.getResultList();
 		} );
+		assertThat( animals ).hasSize( 4 );
 		for ( final Animal animal : animals ) {
-			if ( animal instanceof CatEmbedded ) {
-				final CatEmbedded cat = (CatEmbedded) animal;
-				final List<Kitten> kittens = cat.getKittensEmbeddable().getKittens();
-				assertThat( Hibernate.isInitialized( kittens ) ).isTrue();
-				assertThat( kittens ).hasSizeBetween( 0, 1 );
+			switch ( animal.getType() ) {
+				case CAT:
+					final Cat cat = (Cat) animal;
+					final List<Kitten> kittens = cat.getKittens();
+					assertThat( Hibernate.isInitialized( kittens ) ).isFalse();
+					break;
+				case CAT_EMBEDDED:
+					final CatEmbedded catEmbedded = (CatEmbedded) animal;
+					final List<Kitten> kittensEmbedded = catEmbedded.getKittensEmbeddable().getKittens();
+					assertThat( Hibernate.isInitialized( kittensEmbedded ) ).isTrue();
+					assertThat( kittensEmbedded ).hasSizeBetween( 0, 1 );
 			}
 		}
 	}
@@ -125,6 +150,21 @@ public class JoinFetchInheritanceTest {
 			);
 			return query.getResultList();
 		} );
+		assertThat( animals ).hasSize( 4 );
+		for ( final Animal animal : animals ) {
+			switch ( animal.getType() ) {
+				case CAT:
+					final Cat cat = (Cat) animal;
+					final Kitten kitten = cat.getSingleKitten();
+					assertThat( Hibernate.isInitialized( kitten ) ).isFalse();
+					break;
+				case CAT_EMBEDDED:
+					final CatEmbedded catEmbedded = (CatEmbedded) animal;
+					final Kitten kittenEmbedded = catEmbedded.getKittensEmbeddable().getSingleKitten();
+					assertThat( Hibernate.isInitialized( kittenEmbedded ) ).isTrue();
+					assertThat( kittenEmbedded.getName() ).isEqualTo( "kitten_2" );
+			}
+		}
 	}
 
 	@Entity( name = "Animal" )
@@ -153,45 +193,23 @@ public class JoinFetchInheritanceTest {
 		}
 	}
 
-	@Entity( name = "Dog" )
-	@DiscriminatorValue( "Dog" )
-	public static class Dog extends Animal {
-		@Column( name = "name" )
-		private String name;
-
-		public Dog() {
-		}
-
-		public Dog(Long id, String name) {
-			super( id );
-			this.name = name;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(final String name) {
-			this.name = name;
-		}
-	}
-
 	@Entity( name = "Cat" )
-	@DiscriminatorValue( "Cat" )
+	@DiscriminatorValue( CAT )
 	public static class Cat extends Animal {
-		@ManyToMany( cascade = CascadeType.ALL )
+		@ManyToMany( fetch = FetchType.LAZY )
 		@JoinTable( name = "Cat_Kitten" )
 		public List<Kitten> kittens = new ArrayList<>();
 
-		@ManyToOne
+		@ManyToOne( fetch = FetchType.LAZY )
 		public Kitten singleKitten;
 
 		public Cat() {
 		}
 
-		public Cat(Long id, List<Kitten> kittens) {
+		public Cat(Long id, List<Kitten> kittens, Kitten singleKitten) {
 			super( id );
 			this.kittens = kittens;
+			this.singleKitten = singleKitten;
 		}
 
 		public List<Kitten> getKittens() {
@@ -205,18 +223,19 @@ public class JoinFetchInheritanceTest {
 
 	@Embeddable
 	public static class KittensEmbeddable {
-		@ManyToMany( cascade = CascadeType.ALL )
+		@ManyToMany( fetch = FetchType.LAZY )
 		@JoinTable( name = "CatEmbedded_Kitten" )
 		public List<Kitten> kittens = new ArrayList<>();
 
-		@ManyToOne
+		@ManyToOne( fetch = FetchType.LAZY )
 		public Kitten singleKitten;
 
 		public KittensEmbeddable() {
 		}
 
-		public KittensEmbeddable(List<Kitten> kittens) {
+		public KittensEmbeddable(List<Kitten> kittens, Kitten singleKitten) {
 			this.kittens = kittens;
+			this.singleKitten = singleKitten;
 		}
 
 		public List<Kitten> getKittens() {
@@ -229,7 +248,7 @@ public class JoinFetchInheritanceTest {
 	}
 
 	@Entity( name = "CatEmbedded" )
-	@DiscriminatorValue( "CatEmbedded" )
+	@DiscriminatorValue( CAT_EMBEDDED )
 	public static class CatEmbedded extends Animal {
 		@Embedded
 		private KittensEmbeddable kittensEmbeddable;
@@ -260,6 +279,10 @@ public class JoinFetchInheritanceTest {
 
 		public Kitten(String name) {
 			this.name = name;
+		}
+
+		public String getName() {
+			return name;
 		}
 	}
 }
