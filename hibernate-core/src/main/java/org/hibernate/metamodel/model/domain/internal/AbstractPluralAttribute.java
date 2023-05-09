787f8bc0c31731aa89117c54dbe6ae66568fd275
Marco Belladelli <marco@hibernate.org>
2023-05-09 13:02:48 +0200

    HHH-16555 --wip-- Consider implicit treats when creating SqmJoinable's navigable path

--- hibernate-core/src/main/java/org/hibernate/metamodel/model/domain/internal/AbstractPluralAttribute.java
+++ hibernate-core/src/main/java/org/hibernate/metamodel/model/domain/internal/AbstractPluralAttribute.java
@@ -12,6 +12,7 @@ import java.util.Collection;
 import org.hibernate.metamodel.CollectionClassification;
 import org.hibernate.metamodel.internal.MetadataContext;
 import org.hibernate.metamodel.mapping.CollectionPart;
+import org.hibernate.metamodel.model.domain.EntityDomainType;
 import org.hibernate.metamodel.model.domain.PluralPersistentAttribute;
 import org.hibernate.metamodel.model.domain.SimpleDomainType;
 import org.hibernate.spi.NavigablePath;
@@ -20,6 +21,8 @@ import org.hibernate.query.sqm.internal.SqmMappingModelHelper;
 import org.hibernate.query.sqm.tree.domain.SqmPath;
 import org.hibernate.query.sqm.tree.domain.SqmPluralValuedSimplePath;
 import org.hibernate.type.descriptor.java.JavaType;
+
+import static org.hibernate.query.sqm.spi.SqmCreationHelper.buildSubNavigablePath;
 
 /**
  * @param <D> The (D)eclaring type
@@ -154,6 +157,30 @@ public abstract class AbstractPluralAttribute<D, C, E>
 				lhs,
 				lhs.nodeBuilder()
 		);
+	}
+
+	@Override
+	public NavigablePath createNavigablePath(SqmPath<?> parent, String alias) {
+		if ( parent == null ) {
+			throw new IllegalArgumentException(
+					"`lhs` cannot be null for a sub-navigable reference - " + getName()
+			);
+		}
+		final SqmPathSource<?> parentPathSource = parent.getReferencedPathSource();
+		final NavigablePath navigablePath;
+		if ( parentPathSource instanceof PluralPersistentAttribute<?, ?, ?> ) {
+			navigablePath = parent.getNavigablePath().append( CollectionPart.Nature.ELEMENT.getName() );
+		}
+		else if ( parent.getLhs() == null && parentPathSource != getDeclaringType() &&
+					( (EntityDomainType<?>) parentPathSource ).findPluralAttribute( getName() ) == null ) {
+			// If the parent path is a root and its entity type does not contain the joined attribute
+			// add an implicit treat to the parent's navigable path
+			navigablePath = parent.getNavigablePath().treatAs( getDeclaringType().getTypeName() );
+		}
+		else {
+			navigablePath = parent.getNavigablePath();
+		}
+		return buildSubNavigablePath( navigablePath, getName(), alias );
 	}
 
 	@Override
