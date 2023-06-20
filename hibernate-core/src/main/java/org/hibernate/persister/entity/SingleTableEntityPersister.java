1971c2c6e046df79102420c08c35f1055b99ac78
Marco Belladelli <marco@hibernate.org>
2023-06-20 16:50:19 +0200

    HHH-16798 --wip-- enable treat discriminator for the current entity type

--- hibernate-core/src/main/java/org/hibernate/persister/entity/SingleTableEntityPersister.java
+++ hibernate-core/src/main/java/org/hibernate/persister/entity/SingleTableEntityPersister.java
@@ -677,7 +677,8 @@ public class SingleTableEntityPersister extends AbstractEntityPersister {
 			// Filtering for abstract entities makes no sense, so ignore that
 			// Also, it makes no sense to filter for any of the super types,
 			// as the query will contain a filter for that already anyway
-			if ( !persister.isAbstract() && !isTypeOrSuperType( persister ) && useKind == EntityNameUse.UseKind.TREAT ) {
+			if ( !persister.isAbstract() && ( getSuperMappingType() == null || !getSuperMappingType().isTypeOrSuperType( persister ) )
+					&& useKind == EntityNameUse.UseKind.TREAT ) {
 				containsTreatUse = true;
 				break;
 			}
