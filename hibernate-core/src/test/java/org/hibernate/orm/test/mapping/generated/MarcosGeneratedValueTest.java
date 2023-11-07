196c1712b7a4ff1455a8ef2c2f3523100f89f683
Marco Belladelli <marco@hibernate.org>
2023-11-07 17:12:24 +0100

    --wip-- combie with identifier col + `GetGeneratedKeysDelegate` too. Still neededs:  - add support for _rowid_  - add support for other delegates ( select by unique key, other ?)

--- hibernate-core/src/test/java/org/hibernate/orm/test/mapping/generated/MarcosGeneratedValueTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/generated/MarcosGeneratedValueTest.java
@@ -7,38 +7,68 @@
 package org.hibernate.orm.test.mapping.generated;
 
 import java.util.Calendar;
-import java.util.Date;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.Generated;
 import org.hibernate.generator.EventType;
 
+import org.hibernate.testing.orm.junit.DialectFeatureChecks;
 import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.RequiresDialectFeature;
 import org.hibernate.testing.orm.junit.SessionFactory;
 import org.hibernate.testing.orm.junit.SessionFactoryScope;
 import org.junit.jupiter.api.Test;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-@DomainModel( annotatedClasses = MarcosGeneratedValueTest.TheEntity.class )
+@DomainModel( annotatedClasses = {
+		MarcosGeneratedValueTest.ValuesOnly.class,
+		MarcosGeneratedValueTest.IdentityOnly.class,
+		MarcosGeneratedValueTest.IdentityAndValues.class,
+} )
 @SessionFactory
+@RequiresDialectFeature( feature = DialectFeatureChecks.SupportsIdentityColumns.class )
 public class MarcosGeneratedValueTest {
 	@Test
-	public void testInsertGeneration(SessionFactoryScope scope) {
+	public void testInsertGenerationValuesOnly(SessionFactoryScope scope) {
 		scope.inTransaction( session -> {
-			final TheEntity theEntity = new TheEntity( 1 );
-			session.persist( theEntity );
+			final ValuesOnly entity = new ValuesOnly( 1 );
+			session.persist( entity );
 			session.flush();
-			assertThat( theEntity.getName() ).isEqualTo( "default" );
+			assertThat( entity.getName() ).isEqualTo( "default" );
 		} );
 	}
 
-	@Entity( name = "TheEntity" )
-	public static class TheEntity {
+	@Test
+	public void testInsertGenerationIdentityOnly(SessionFactoryScope scope) {
+		scope.inTransaction( session -> {
+			final IdentityOnly entity = new IdentityOnly();
+			session.persist( entity );
+			session.flush();
+			assertThat( entity.getId() ).isNotNull();
+			assertThat( entity.getName() ).isNull();
+		} );
+	}
+
+	@Test
+	public void testInsertGenerationValuesAndIdentity(SessionFactoryScope scope) {
+		scope.inTransaction( session -> {
+			final IdentityAndValues entity = new IdentityAndValues();
+			session.persist( entity );
+			session.flush();
+			assertThat( entity.getId() ).isNotNull();
+			assertThat( entity.getName() ).isEqualTo( "default" );
+		} );
+	}
+
+	@Entity( name = "ValuesOnly" )
+	public static class ValuesOnly {
 		@Id
 		private Integer id;
 
@@ -50,11 +80,61 @@ public class MarcosGeneratedValueTest {
 		@ColumnDefault( "CURRENT_TIMESTAMP" )
 		private Calendar updateDate;
 
-		public TheEntity() {
+		public ValuesOnly() {
 		}
 
-		private TheEntity(Integer id) {
+		private ValuesOnly(Integer id) {
 			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public Calendar getUpdateDate() {
+			return updateDate;
+		}
+	}
+
+	@Entity( name = "IdentityOnly" )
+	public static class IdentityOnly {
+		@Id
+		@GeneratedValue( strategy = GenerationType.IDENTITY )
+		private Integer id;
+
+		private String name;
+
+		private Calendar updateDate;
+
+		public Integer getId() {
+			return id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public Calendar getUpdateDate() {
+			return updateDate;
+		}
+	}
+
+	@Entity( name = "IdentityAndValues" )
+	public static class IdentityAndValues {
+		@Id
+		@GeneratedValue( strategy = GenerationType.IDENTITY )
+		private Integer id;
+
+		@Generated( event = EventType.INSERT )
+		@ColumnDefault( "'default'" )
+		private String name;
+
+		@Generated( event = EventType.UPDATE )
+		@ColumnDefault( "CURRENT_TIMESTAMP" )
+		private Calendar updateDate;
+
+		public Integer getId() {
+			return id;
 		}
 
 		public String getName() {
