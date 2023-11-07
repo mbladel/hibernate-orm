7f3839bccceb665088ee64eb0253c034c49385e2
Marco Belladelli <marco@hibernate.org>
2023-11-07 15:39:04 +0100

    --wip-- first poc for selecting attributes in returning works, needed:  - combine identity + attribute selection  - add support for other delegates (getGeneratedValues() api, select by unique key)  - add support for _rowid_ retrieval

--- hibernate-core/src/main/java/org/hibernate/id/insert/TableInsertReturningBuilder.java
+++ hibernate-core/src/main/java/org/hibernate/id/insert/TableInsertReturningBuilder.java
@@ -7,10 +7,16 @@
 package org.hibernate.id.insert;
 
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.id.PostInsertIdentityPersister;
 import org.hibernate.metamodel.mapping.BasicEntityIdentifierMapping;
+import org.hibernate.metamodel.mapping.BasicValuedModelPart;
+import org.hibernate.metamodel.mapping.ValuedModelPart;
+import org.hibernate.persister.entity.EntityPersister;
+import org.hibernate.persister.entity.mutation.EntityMutationTarget;
 import org.hibernate.sql.ast.tree.expression.ColumnReference;
 import org.hibernate.sql.model.ast.TableInsert;
 import org.hibernate.sql.model.ast.builder.AbstractTableInsertBuilder;
@@ -33,13 +39,17 @@ public class TableInsertReturningBuilder extends AbstractTableInsertBuilder {
 
 	@Override
 	public TableInsert buildMutation() {
-		final BasicEntityIdentifierMapping identifierMapping =
-				(BasicEntityIdentifierMapping) getMutationTarget().getIdentifierMapping();
+		final List<? extends ValuedModelPart> insertGeneratedProperties = getMutationTarget().getInsertGeneratedProperties();
+		final List<ColumnReference> generatedColumns = insertGeneratedProperties.stream().map( prop -> {
+			assert prop instanceof BasicValuedModelPart; // todo marco : handle this differently
+			return new ColumnReference( getMutatingTable(), ( (BasicValuedModelPart) prop ) );
+		} ).collect( Collectors.toList() );
+
 		return new TableInsertStandard(
 				getMutatingTable(),
 				getMutationTarget(),
 				combine( getValueBindingList(), getKeyBindingList(), getLobValueBindingList() ),
-				Collections.singletonList( new ColumnReference( getMutatingTable(), identifierMapping ) ),
+				generatedColumns,
 				getParameters()
 		);
 	}
