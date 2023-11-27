d51092129aceb306f248b0f29a93445fe3684bd8
Marco Belladelli <marco@hibernate.org>
2023-11-27 15:07:55 +0100

    fixup! HHH-17468 --wip-- test

--- hibernate-core/src/test/java/org/hibernate/orm/test/mapping/collections/ReadOnlyCollectionsTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/collections/ReadOnlyCollectionsTest.java
@@ -9,19 +9,18 @@ package org.hibernate.orm.test.mapping.collections;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.annotations.JoinFormula;
-
 import org.hibernate.testing.orm.domain.gambit.BasicEntity;
 import org.hibernate.testing.orm.junit.DomainModel;
 import org.hibernate.testing.orm.junit.SessionFactory;
 import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
@@ -40,23 +39,48 @@ import static org.assertj.core.api.Assertions.assertThat;
 } )
 @SessionFactory
 public class ReadOnlyCollectionsTest {
+	@BeforeAll
+	public void setUp(SessionFactoryScope scope) {
+		scope.inTransaction( session -> {
+			session.persist( new TargetEntity( 1L ) );
+			session.persist( new TargetEntity( 2L ) );
+			session.persist( new BasicEntity( 1, "be1" ) );
+			session.persist( new BasicEntity( 2, "be2" ) );
+			session.persist( new CollectionsContainer( 1L, "container_1" ) );
+			session.persist( new CollectionsContainer( 2L, "container_2" ) );
+		} );
+		scope.inTransaction( session -> {
+			session.createNativeMutationQuery( "insert into otm_join_table(container_id, basic_id) values (2,1)" ).executeUpdate();
+			session.createNativeMutationQuery( "insert into otm_join_table(container_id, basic_id) values (2,2)" ).executeUpdate();
+			session.createNativeMutationQuery( "insert into mtm_join_table(container_id, basic_id) values (2,1)" ).executeUpdate();
+			session.createNativeMutationQuery( "insert into mtm_join_table(container_id, basic_id) values (2,2)" ).executeUpdate();
+			session.createNativeMutationQuery( "insert into el_col_table(container_id, long_value) values (2,1)" ).executeUpdate();
+			session.createNativeMutationQuery( "insert into el_col_table(container_id, long_value) values (2,2)" ).executeUpdate();
+			session.createNativeMutationQuery( "insert into TargetEntity(id, container_id) values (98,2)" ).executeUpdate();
+			session.createNativeMutationQuery( "insert into TargetEntity(id, container_id) values (99,2)" ).executeUpdate();
+		} );
+	}
+
+	@AfterAll
+	public void tearDown(SessionFactoryScope scope) {
+		scope.inTransaction( session -> {
+			session.createMutationQuery( "delete from TargetEntity" ).executeUpdate();
+			session.createMutationQuery( "delete from CollectionsContainer" ).executeUpdate();
+			session.createMutationQuery( "delete from BasicEntity" ).executeUpdate();
+		} );
+	}
+
 	@Test
 	public void testInsert(SessionFactoryScope scope) {
-		// todo marco : would be nice to test deletes too !
+		scope.inTransaction( session -> {
+			final CollectionsContainer container = session.find( CollectionsContainer.class, 1L );
+			container.name = "container_1_updated";
 
-		final Long containerId = scope.fromTransaction( session -> {
-			final TargetEntity te1 = new TargetEntity();
-			final TargetEntity te2 = new TargetEntity();
-			session.persist( te1 );
-			session.persist( te2 );
-			final BasicEntity be1 = new BasicEntity( 1, "b1" );
-			final BasicEntity be2 = new BasicEntity( 2, "b2" );
-			session.persist( be1 );
-			session.persist( be2 );
-			final CollectionsContainer container = new CollectionsContainer();
+			container.oneToMany.add( session.find( TargetEntity.class, 1L ) );
+			container.oneToMany.add( session.find( TargetEntity.class, 2L ) );
 
-			container.oneToMany.add( te1 );
-			container.oneToMany.add( te2 );
+			final BasicEntity be1 = session.find( BasicEntity.class, 1 );
+			final BasicEntity be2 = session.find( BasicEntity.class, 2 );
 
 			container.oneToManyJoinTable.add( be1 );
 			container.oneToManyJoinTable.add( be2 );
@@ -64,80 +88,55 @@ public class ReadOnlyCollectionsTest {
 			container.manyToMany.add( be1 );
 			container.manyToMany.add( be2 );
 
-			// This leads to an error: value column is not in insert statement, yet we still try to bind it
-			container.elementCollection.add( 1L );
-			container.elementCollection.add( 2L );
-
 			container.elementCollectionTable.add( 1L );
 			container.elementCollectionTable.add( 2L );
-			session.persist( container );
-
-			session.flush();
-			return container.id;
 		} );
 
 		scope.inSession( session -> {
-			final CollectionsContainer container = session.find( CollectionsContainer.class, containerId );
+			final CollectionsContainer container = session.find( CollectionsContainer.class, 1L );
+			// No elements should have been added to the collections
+			assertThat( container.name ).isEqualTo( "container_1_updated" );
 			assertThat( container.oneToMany ).hasSize( 0 );
 			assertThat( container.oneToManyJoinTable ).hasSize( 0 );
 			assertThat( container.manyToMany ).hasSize( 0 );
-			assertThat( container.elementCollection ).hasSize( 0 );
 			assertThat( container.elementCollectionTable ).hasSize( 0 );
 		} );
 	}
 
 	@Test
-	public void testUpdate(SessionFactoryScope scope) {
-		final Long containerId = scope.fromTransaction( session -> {
-			final CollectionsContainer container = new CollectionsContainer();
-			session.persist( container );
-			session.flush();
-			return container.id;
+	public void testDelete(SessionFactoryScope scope) {
+		scope.inTransaction( session -> {
+			final CollectionsContainer container = session.find( CollectionsContainer.class, 2L );
+			assertThat( container.oneToMany ).hasSize( 2 );
+			assertThat( container.oneToManyJoinTable ).hasSize( 2 );
+			assertThat( container.manyToMany ).hasSize( 2 );
+			assertThat( container.elementCollectionTable ).hasSize( 2 );
+
+			container.name = "container_2_updated";
+
+			container.oneToMany.clear();
+			container.oneToManyJoinTable.clear();
+			container.manyToMany.clear();
+			container.elementCollectionTable.clear();
 		} );
 
 		scope.inTransaction( session -> {
-			final TargetEntity te1 = new TargetEntity();
-			final TargetEntity te2 = new TargetEntity();
-			session.persist( te1 );
-			session.persist( te2 );
-			final BasicEntity be1 = new BasicEntity( 3, "b1" );
-			final BasicEntity be2 = new BasicEntity( 4, "b2" );
-			session.persist( be1 );
-			session.persist( be2 );
-			final CollectionsContainer container = session.find( CollectionsContainer.class, containerId );
-
-			container.oneToMany.add( te1 );
-			container.oneToMany.add( te2 );
-
-			container.oneToManyJoinTable.add( be1 );
-			container.oneToManyJoinTable.add( be2 );
-
-			container.manyToMany.add( be1 );
-			container.manyToMany.add( be2 );
-
-			// This leads to an error: value column is not in insert statement, yet we still try to bind it
-			container.elementCollection.add( 1L );
-			container.elementCollection.add( 2L );
-
-			container.elementCollectionTable.add( 1L );
-			container.elementCollectionTable.add( 2L );
-			session.persist( container );
-		} );
-		scope.inSession( session -> {
-			final CollectionsContainer container = session.find( CollectionsContainer.class, containerId );
-			assertThat( container.oneToMany ).hasSize( 0 );
-			assertThat( container.oneToManyJoinTable ).hasSize( 0 );
-			assertThat( container.manyToMany ).hasSize( 0 );
-			assertThat( container.elementCollection ).hasSize( 0 );
-			assertThat( container.elementCollectionTable ).hasSize( 0 );
+			final CollectionsContainer container = session.find( CollectionsContainer.class, 2L );
+			// No elements should have been removed from the collections
+			assertThat( container.name ).isEqualTo( "container_2_updated" );
+			assertThat( container.oneToMany ).hasSize( 2 );
+			assertThat( container.oneToManyJoinTable ).hasSize( 2 );
+			assertThat( container.manyToMany ).hasSize( 2 );
+			assertThat( container.elementCollectionTable ).hasSize( 2 );
 		} );
 	}
 
 	@Entity( name = "CollectionsContainer" )
 	public static class CollectionsContainer {
 		@Id
-		@GeneratedValue
 		private Long id;
+
+		private String name;
 
 		@OneToMany
 		@JoinColumn( name = "container_id", insertable = false, updatable = false )
@@ -145,33 +144,44 @@ public class ReadOnlyCollectionsTest {
 
 		@OneToMany
 		@JoinTable(
-				name = "one_to_many_join_table",
-				joinColumns = @JoinColumn( name = "basic_id", insertable = false, updatable = false ),
-				inverseJoinColumns = @JoinColumn( name = "container_id", insertable = false, updatable = false )
+				name = "otm_join_table",
+				joinColumns = @JoinColumn( name = "container_id", insertable = false, updatable = false ),
+				inverseJoinColumns = @JoinColumn( name = "basic_id", insertable = false, updatable = false )
 		)
 		private List<BasicEntity> oneToManyJoinTable = new ArrayList<>();
 
 		@ManyToMany
 		@JoinTable(
-				name = "many_to_many_join_table",
+				name = "mtm_join_table",
 				joinColumns = @JoinColumn( name = "container_id", insertable = false, updatable = false ),
 				inverseJoinColumns = @JoinColumn( name = "basic_id", insertable = false, updatable = false )
 		)
 		private List<BasicEntity> manyToMany = new ArrayList<>();
 
 		@ElementCollection
-		@Column( name = "element_collection_col", insertable = false, updatable = false )
-		private List<Long> elementCollection = new ArrayList<>();
-
-		@ElementCollection
+		@Column( name = "long_value", insertable = false, updatable = false )
 		@CollectionTable( name = "el_col_table", joinColumns = @JoinColumn( name = "container_id", insertable = false, updatable = false ) )
 		private List<Long> elementCollectionTable = new ArrayList<>();
+
+		public CollectionsContainer() {
+		}
+
+		public CollectionsContainer(Long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
 	}
 
 	@Entity( name = "TargetEntity" )
 	public static class TargetEntity {
 		@Id
-		@GeneratedValue
 		private Long id;
+
+		public TargetEntity() {
+		}
+
+		public TargetEntity(Long id) {
+			this.id = id;
+		}
 	}
 }
