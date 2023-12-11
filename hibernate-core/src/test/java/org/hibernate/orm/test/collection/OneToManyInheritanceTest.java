307a43d889184c2c84395ca8eb972b0895ce6f0f
Marco Belladelli <marco@hibernate.org>
2023-12-11 10:49:48 +0100

    fixup! HHH-17483 Add test for issue

--- hibernate-core/src/test/java/org/hibernate/orm/test/collection/OneToManyInheritanceTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/collection/OneToManyInheritanceTest.java
@@ -1,17 +1,13 @@
 package org.hibernate.orm.test.collection;
 
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
-import org.hibernate.testing.TestForIssue;
 import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.Jira;
 import org.hibernate.testing.orm.junit.SessionFactory;
 import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -26,41 +22,30 @@ import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@DomainModel(
-		annotatedClasses = {
-				OneToManyInheritanceTest.Food.class,
-				OneToManyInheritanceTest.Cheese.class,
-				OneToManyInheritanceTest.SmellyCheese.class,
-				OneToManyInheritanceTest.Refrigerator.class
-		}
-)
+@DomainModel( annotatedClasses = {
+		OneToManyInheritanceTest.Food.class,
+		OneToManyInheritanceTest.Cheese.class,
+		OneToManyInheritanceTest.SmellyCheese.class,
+		OneToManyInheritanceTest.Refrigerator.class
+} )
 @SessionFactory
-@TestForIssue(jiraKey = "HHH-17483")
+@Jira( "https://hibernate.atlassian.net/browse/HHH-17483" )
 public class OneToManyInheritanceTest {
-
 	private static final Integer REFRIGERATOR_ID = 42;
 
 	@BeforeAll
 	public void prepare(SessionFactoryScope scope) {
 		scope.inTransaction( session -> {
-			Cheese cheese1 = new Cheese( 1, "Roquefort" );
-			SmellyCheese cheese2 = new SmellyCheese( 2, "Maroilles" );
-			SmellyCheese cheese3 = new SmellyCheese( 3, "Vieux Lille" );
-			Set<Cheese> cheeses = new HashSet<>();
-			cheeses.add( cheese1 );
-			cheeses.add( cheese2 );
-			cheeses.add( cheese3 );
-			
-			Refrigerator refrigerator = new Refrigerator( REFRIGERATOR_ID, "Fridge", cheeses );
-			
-			cheese1.setRefrigerator( refrigerator );
-			cheese2.setRefrigerator( refrigerator );
-			cheese3.setRefrigerator( refrigerator );
-			
+			final Refrigerator refrigerator = new Refrigerator( REFRIGERATOR_ID, "Fridge" );
+			final Cheese cheese1 = new Cheese( 1, "Roquefort", refrigerator );
+			final SmellyCheese cheese2 = new SmellyCheese( 2, "Maroilles", refrigerator );
+			final SmellyCheese cheese3 = new SmellyCheese( 3, "Vieux Lille", refrigerator );
+			refrigerator.getCheeses().add( cheese1 );
+			refrigerator.getCheeses().add( cheese2 );
+			refrigerator.getCheeses().add( cheese3 );
 			session.persist( cheese1 );
 			session.persist( cheese2 );
 			session.persist( cheese3 );
@@ -68,132 +53,102 @@ public class OneToManyInheritanceTest {
 		} );
 	}
 
-	@Test
-	public void loadCollection(SessionFactoryScope scope) {
+	@AfterAll
+	public void tearDown(SessionFactoryScope scope) {
 		scope.inTransaction( session -> {
-			Refrigerator refrigerator = session.find( Refrigerator.class, REFRIGERATOR_ID );
-			Assertions.assertThat( refrigerator.getCheeses() ).hasSize( 3 );
+			session.createMutationQuery( "delete from Food" ).executeUpdate();
+			session.createMutationQuery( "delete from Refrigerator" ).executeUpdate();
 		} );
 	}
 
-	@Entity(name = "Refrigerator")
-	@Table(name = "Refrigerator")
-	static class Refrigerator {
+	@Test
+	public void testCollectionLoad(SessionFactoryScope scope) {
+		scope.inTransaction( session -> {
+			final Refrigerator refrigerator = session.find( Refrigerator.class, REFRIGERATOR_ID );
+			assertThat( refrigerator.getCheeses() ).hasSize( 3 );
+		} );
+	}
+
+	@Test
+	public void testCollectionQueryFetch(SessionFactoryScope scope) {
+		scope.inTransaction( session -> {
+			final Refrigerator refrigerator = session.createQuery(
+					"from Refrigerator r join fetch r.cheeses where r.id = :id",
+					Refrigerator.class
+			).setParameter( "id", REFRIGERATOR_ID ).getSingleResult();
+			assertThat( refrigerator.getCheeses() ).hasSize( 3 );
+		} );
+	}
+
+	@Entity( name = "Refrigerator" )
+	public static class Refrigerator {
 		@Id
 		private Integer id;
+
 		private String name;
 
-		@OneToMany(targetEntity = Cheese.class, mappedBy = "refrigerator")
+		@OneToMany( mappedBy = "refrigerator" )
 		private Set<Cheese> cheeses;
 
 		public Refrigerator() {
 		}
 
-		public Refrigerator(Integer id, String name, Collection<Cheese> cheeses) {
+		public Refrigerator(Integer id, String name) {
 			this.id = id;
 			this.name = name;
-			this.cheeses = new HashSet<>( cheeses );
-		}
-
-		public Integer getId() {
-			return id;
-		}
-
-		public void setId(Integer id) {
-			this.id = id;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
+			this.cheeses = new HashSet<>();
 		}
 
 		public Set<Cheese> getCheeses() {
 			return cheeses;
 		}
-
-		public void setCheeses(Set<Cheese> cheeses) {
-			this.cheeses = cheeses;
-		}
-
 	}
 
-	@Entity(name = "Food")
-	@Table(name = "Food")
-	@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-	@DiscriminatorColumn(discriminatorType = DiscriminatorType.STRING, name = "type")
-	@DiscriminatorValue(value = "Food")
-	static class Food {
-
+	@Entity( name = "Food" )
+	@Inheritance( strategy = InheritanceType.SINGLE_TABLE )
+	@DiscriminatorColumn( discriminatorType = DiscriminatorType.STRING, name = "food_type" )
+	@DiscriminatorValue( value = "Food" )
+	public static class Food {
 		@Id
 		private Integer id;
 
 		private String name;
-		
-		@ManyToOne(fetch = FetchType.LAZY)
-		@Fetch(FetchMode.SELECT)
-		@JoinColumn(name = "refrigerator_id")
+
+		@ManyToOne( fetch = FetchType.LAZY )
+		@JoinColumn( name = "refrigerator_id" )
 		private Refrigerator refrigerator;
 
 		public Food() {
 		}
 
-		public Food(Integer id, String name) {
+		public Food(Integer id, String name, Refrigerator refrigerator) {
 			this.id = id;
 			this.name = name;
-		}
-
-		public Integer getId() {
-			return id;
-		}
-
-		public void setId(Integer id) {
-			this.id = id;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public Refrigerator getRefrigerator() {
-			return refrigerator;
-		}
-
-		public void setRefrigerator(Refrigerator refrigerator) {
 			this.refrigerator = refrigerator;
 		}
 	}
-	
-	@Entity(name = "Cheese")
-	@DiscriminatorValue("Cheese")
-	static class Cheese extends Food {
 
+	@Entity( name = "Cheese" )
+	@DiscriminatorValue( "Cheese" )
+	public static class Cheese extends Food {
 		public Cheese() {
 			super();
 		}
 
-		public Cheese(Integer id, String name) {
-			super(id, name);
+		public Cheese(Integer id, String name, Refrigerator refrigerator) {
+			super( id, name, refrigerator );
 		}
 	}
-	
-	@Entity(name = "SmellyCheese")
-	@DiscriminatorValue("SmellyCheese")
-	static class SmellyCheese extends Cheese {
 
+	@Entity( name = "SmellyCheese" )
+	@DiscriminatorValue( "SmellyCheese" )
+	public static class SmellyCheese extends Cheese {
 		public SmellyCheese() {
 			super();
 		}
 
-		public SmellyCheese(Integer id, String name) {
-			super(id, name);
+		public SmellyCheese(Integer id, String name, Refrigerator refrigerator) {
+			super( id, name, refrigerator );
 		}
 	}
 }
