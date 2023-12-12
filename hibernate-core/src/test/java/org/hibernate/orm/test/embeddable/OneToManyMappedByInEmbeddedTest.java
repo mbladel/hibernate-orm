d36299ecd93cda1764c2a1135bd0a0fadea3f14f
Marco Belladelli <marco@hibernate.org>
2023-12-12 11:51:47 +0100

    HHH-17528 --wip-- test changes

--- hibernate-core/src/test/java/org/hibernate/orm/test/embeddable/OneToManyMappedByInEmbeddedTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/embeddable/OneToManyMappedByInEmbeddedTest.java
@@ -13,12 +13,13 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OrderBy;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DomainModel( annotatedClasses = {
 		OneToManyMappedByInEmbeddedTest.EntityA.class,
@@ -27,132 +28,119 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @SessionFactory
 public class OneToManyMappedByInEmbeddedTest {
 	@BeforeAll
-	public void prepare(SessionFactoryScope scope) {
+	public void setUp(SessionFactoryScope scope) {
 		scope.inTransaction( session -> {
-			final EntityA entityA = new EntityA( 1 );
-
-			final EmbeddedValueInA embeddedValueInA = new EmbeddedValueInA();
-
-			final EntityC entityC = new EntityC();
-			entityC.setId( 1 );
-			entityC.setName( "testName" );
-
-			final EntityC entityC1 = new EntityC();
-			entityC1.setName( "testName1" );
-			entityC1.setId( 2 );
-
-			embeddedValueInA.setEntityCList( List.of( entityC, entityC1 ) );
-			entityA.setEmbedded( embeddedValueInA );
-
-			session.persist( entityA );
+			final EntityC entityC1 = new EntityC( "entityc_1" );
+			final EntityC entityC2 = new EntityC( "entityc_2" );
+			final EntityC entityC3 = new EntityC( "entityc_3" );
+			final EntityA entityA1 = new EntityA( 1, "entitya_1", new EmbeddedValue( List.of( entityC1, entityC2 ) ) );
+			final EntityA entityA2 = new EntityA( 2, "entitya_2", new EmbeddedValue( List.of( entityC3 ) ) );
+			session.persist( entityA1 );
+			session.persist( entityA2 );
 		} );
 	}
 
 	@Test
-	public void testEmbeddableWithOneToManyLoadBefore(SessionFactoryScope scope) {
+	public void testSelectEntity(SessionFactoryScope scope) {
 		scope.inTransaction( session -> {
-			final EntityA entityA = session.get( EntityA.class, 1 );
-			assertThat( entityA ).isNotNull();
-			final Object entityA1 = session.createQuery( "select a.embedded from EntityA a where a.id = 1" )
-					.getSingleResult();
-			assertThat( entityA ).isNotNull();
+			final EntityA entityA = session.createQuery(
+					"select a from EntityA a where a.id = 1",
+					EntityA.class
+			).getSingleResult();
+			assertThat( entityA.getEmbedded().getEntityCList().stream().map( EntityC::getName ) ).containsOnly(
+					"entityc_1",
+					"entityc_2"
+			);
+			// test orphan removal
+			entityA.getEmbedded().getEntityCList().clear();
+		} );
+		scope.inSession( session -> {
+			final List<EntityC> entityCList = session.createQuery( "from EntityC", EntityC.class ).getResultList();
+			assertThat( entityCList.stream().map( EntityC::getName ) ).doesNotContain( "entityc_1", "entityc_2" );
 		} );
 	}
 
 	@Test
-	public void testEmbeddableWithoutOneToMany(SessionFactoryScope scope) {
+	public void testSelectEmbedded(SessionFactoryScope scope) {
 		scope.inTransaction( session -> {
-			final Object entityB = session.createQuery( "select b.embedded from EntityB b where b.id = 1" )
-					.getSingleResult();
-			assertThat( entityB ).isNotNull();
+			final EmbeddedValue embedded = session.createQuery(
+					"select a.embedded from EntityA a where a.id = 2",
+					EmbeddedValue.class
+			).getSingleResult();
+			assertThat( embedded.getEntityCList() ).hasSize( 1 );
+			assertThat( embedded.getEntityCList().stream().map( EntityC::getName ) ).containsOnly( "entityc_3" );
+			// test orphan removal
+			embedded.getEntityCList().clear();
+		} );
+		scope.inSession( session -> {
+			final List<EntityC> entityCList = session.createQuery( "from EntityC", EntityC.class ).getResultList();
+			assertThat( entityCList.stream().map( EntityC::getName ) ).doesNotContain( "entityc_3" );
 		} );
 	}
-
-	@Test
-	public void testEmbeddableWithOneToMany(SessionFactoryScope scope) {
-		scope.inTransaction( session -> {
-			final Object entityA = session.createQuery( "select a.embedded from EntityA a where a.id = 1" )
-					.getSingleResult();
-			assertThat( entityA ).isNotNull();
-		} );
-	}
-
 
 	@Entity( name = "EntityA" )
 	public static class EntityA {
 		@Id
 		private Integer id;
 
+		private String name;
+
 		@Embedded
-		private EmbeddedValueInA embedded = new EmbeddedValueInA();
+		private EmbeddedValue embedded;
 
 		public EntityA() {
 		}
 
-		private EntityA(Integer id) {
+		private EntityA(Integer id, String name, EmbeddedValue embedded) {
 			this.id = id;
+			this.name = name;
+			this.embedded = embedded;
 		}
 
 		public Integer getId() {
 			return id;
 		}
 
-		public EmbeddedValueInA getEmbedded() {
+		public EmbeddedValue getEmbedded() {
 			return embedded;
-		}
-
-		public void setEmbedded(EmbeddedValueInA embedded) {
-			this.embedded = embedded;
 		}
 	}
 
 	@Embeddable
-	public static class EmbeddedValueInA implements Serializable {
-		private String testString;
-
-		@OneToMany( cascade = CascadeType.ALL, orphanRemoval = true )
+	public static class EmbeddedValue implements Serializable {
+		@OneToMany( cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER )
 		@JoinColumn( name = "entityA_id" )
-		@OrderBy( "id" )
 		private List<EntityC> entityCList;
 
-		public List<EntityC> getEntityCList() {
-			return entityCList;
+		public EmbeddedValue() {
 		}
 
-		public void setEntityCList(List<EntityC> entityCList) {
+		public EmbeddedValue(List<EntityC> entityCList) {
 			this.entityCList = entityCList;
 		}
 
-		public String getTestString() {
-			return testString;
-		}
-
-		public void setTestString(String testString) {
-			this.testString = testString;
+		public List<EntityC> getEntityCList() {
+			return entityCList;
 		}
 	}
 
 	@Entity( name = "EntityC" )
 	public static class EntityC {
 		@Id
+		@GeneratedValue
 		private Integer id;
 
 		private String name;
 
-		public Integer getId() {
-			return id;
+		public EntityC() {
 		}
 
-		public void setId(Integer id) {
-			this.id = id;
+		public EntityC(String name) {
+			this.name = name;
 		}
 
 		public String getName() {
 			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
 		}
 	}
 }
