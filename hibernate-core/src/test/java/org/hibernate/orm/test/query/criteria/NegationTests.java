5dccfceadb9377a2ef241b4203af221e72177511
Marco Belladelli <marco@hibernate.org>
2024-01-31 11:04:51 +0100

    HHH-17493 Add test for issue

--- hibernate-core/src/test/java/org/hibernate/orm/test/query/criteria/NegationTests.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/query/criteria/NegationTests.java
@@ -8,7 +8,9 @@ package org.hibernate.orm.test.query.criteria;
 
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+import org.hibernate.query.criteria.JpaCriteriaQuery;
 import org.hibernate.query.criteria.JpaPredicate;
+import org.hibernate.query.criteria.JpaRoot;
 
 import org.hibernate.testing.orm.domain.gambit.BasicEntity;
 import org.hibernate.testing.orm.junit.DomainModel;
@@ -16,40 +18,76 @@ import org.hibernate.testing.orm.junit.ServiceRegistry;
 import org.hibernate.testing.orm.junit.SessionFactory;
 import org.hibernate.testing.orm.junit.SessionFactoryScope;
 import org.hibernate.testing.orm.junit.Setting;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.criteria.Predicate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Steve Ebersole
  */
-@ServiceRegistry(
-		settings = @Setting( name = AvailableSettings.JPA_QUERY_COMPLIANCE, value = "true" )
-)
 @DomainModel( annotatedClasses = BasicEntity.class )
-@SessionFactory
 public class NegationTests {
 	@Test
-	public void	simpleTest(SessionFactoryScope scope) {
-		scope.inTransaction( (session) -> {
-			final HibernateCriteriaBuilder nodeBuilder = session.getFactory().getCriteriaBuilder();
-			final JpaPredicate equality = nodeBuilder.equal( nodeBuilder.literal( 1 ), nodeBuilder.literal( 1 ) );
-			final JpaPredicate inequality = equality.not();
-			assertThat( equality ).isNotSameAs( inequality );
-			assertThat( equality.isNegated() ).isFalse();
-			assertThat( inequality.isNegated() ).isTrue();
+	@ServiceRegistry( settings = @Setting( name = AvailableSettings.JPA_QUERY_COMPLIANCE, value = "false" ) )
+	@SessionFactory
+	public void testComplianceDisabled(SessionFactoryScope scope) {
+		scope.inTransaction( session -> session.persist( new BasicEntity( 1, "entity_1" ) ) );
+		testPredicateNegation( scope, false,false );
+		testPredicateNegation( scope, false, true );
+	}
+
+	@Test
+	@ServiceRegistry( settings = @Setting( name = AvailableSettings.JPA_QUERY_COMPLIANCE, value = "true" ) )
+	@SessionFactory
+	public void testComplianceEnabled(SessionFactoryScope scope) {
+		scope.inTransaction( session -> session.persist( new BasicEntity( 1, "entity_1" ) ) );
+		testPredicateNegation( scope, true,false );
+		testPredicateNegation( scope, true, true );
+	}
+
+	private void testPredicateNegation(SessionFactoryScope scope, boolean compliance, boolean doubleNegation) {
+		// single comparison
+		executeQuery( scope, doubleNegation, (cb, cq, root) -> {
+			final JpaPredicate comparison = cb.equal( root.get( "id" ), 1 );
+			final JpaPredicate negated = doubleNegation ? comparison.not().not() : comparison.not();
+			assertThat( negated.isNegated() ).isEqualTo( compliance && !doubleNegation );
+			if ( compliance ) {
+				assertThat( negated ).isNotSameAs( comparison );
+			}
+			else {
+				assertThat( negated ).isSameAs( comparison );
+			}
+			// return original predicate when compliance is disabled
+			return compliance ? negated : comparison;
+		} );
+		// junction predicate
+		executeQuery( scope, doubleNegation, (cb, cq, root) -> {
+			final JpaPredicate junction = cb.or(
+					cb.equal( root.get( "id" ), 1 ),
+					cb.equal( root.get( "data" ), "entity_1" )
+			);
+			// always need to use the returned predicate
+			final JpaPredicate negated = doubleNegation ? junction.not().not() : junction.not();
+			assertThat( negated ).isNotSameAs( junction );
+			assertThat( negated.isNegated() ).isEqualTo( doubleNegation );
+			return negated;
 		} );
 	}
 
-	@BeforeEach
-	public void createTestData(SessionFactoryScope scope) {
-		scope.inTransaction( (session) -> session.persist( new BasicEntity( 1, "abc" ) ) );
+	public interface PredicateProducer {
+		Predicate accept(HibernateCriteriaBuilder cb, JpaCriteriaQuery<BasicEntity> cq, JpaRoot<BasicEntity> root);
 	}
 
-	@AfterEach
-	public void dropTestData(SessionFactoryScope scope) {
-		scope.inTransaction( (session) -> session.createQuery( "delete BasicEntity" ).executeUpdate() );
+	private void executeQuery(SessionFactoryScope scope, boolean hasResult, PredicateProducer predicateProducer) {
+		scope.inTransaction( session -> {
+			final HibernateCriteriaBuilder cb = session.getCriteriaBuilder();
+			final JpaCriteriaQuery<BasicEntity> cq = cb.createQuery( BasicEntity.class );
+			final JpaRoot<BasicEntity> root = cq.from( BasicEntity.class );
+			assertThat( session.createQuery(
+					cq.where( predicateProducer.accept( cb, cq, root ) )
+			).getResultList() ).hasSize( hasResult ? 1 : 0 );
+		} );
 	}
 }
