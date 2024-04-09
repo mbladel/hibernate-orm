bdf6c26a471e5af2cc41fbf827be97f38460d1f3
Marco Belladelli <marcobladel@gmail.com>
2024-04-09 14:37:57 +0200

    --wip-- basic implementation works, needs more testing
    - more testing associations
    - TDB if we want to add the embeddable discriminator to association queries
      - it's not strictly necessary, because we skip initialization / avoid persisting anyway, but maybe we should?

--- hibernate-core/src/test/java/org/hibernate/orm/test/inheritance/embeddable/EmbeddableInheritanceAssciationsTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/inheritance/embeddable/EmbeddableInheritanceAssciationsTest.java
@@ -20,6 +20,7 @@ import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
@@ -43,11 +44,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SessionFactory
 public class EmbeddableInheritanceAssciationsTest {
 	@Test
-	public void testToOne(SessionFactoryScope scope) {
+	public void testManyToOne(SessionFactoryScope scope) {
 		scope.inTransaction( session -> {
 			// insert
 			final AssociatedEntity associated = session.find( AssociatedEntity.class, 1L );
 			session.persist( new TestEntity( 1L, new AssociationChildOne( "embeddable_1", associated ) ) );
+		} );
+		scope.inTransaction( session -> {
+			// queries
+			final AssociatedEntity result = session.createQuery(
+					"select embeddable.manyToOne from TestEntity where id = 1",
+					AssociatedEntity.class
+			).getSingleResult();
+			assertThat( result.getId() ).isEqualTo( 1L );
+			assertThat( result.getName() ).isEqualTo( "associated_1" );
 		} );
 		scope.inTransaction( session -> {
 			// find
@@ -71,7 +81,7 @@ public class EmbeddableInheritanceAssciationsTest {
 	}
 
 	@Test
-	public void testToOneSubtype(SessionFactoryScope scope) {
+	public void testManyToOneSubtype(SessionFactoryScope scope) {
 		scope.inTransaction( session -> {
 			// insert
 			final AssociatedEntity associated = session.find( AssociatedEntity.class, 2L );
@@ -88,6 +98,100 @@ public class EmbeddableInheritanceAssciationsTest {
 		} );
 	}
 
+	@Test
+	public void testManyToMany(SessionFactoryScope scope) {
+		scope.inTransaction( session -> {
+			// insert
+			final AssociationChildTwo associationChildTwo = new AssociationChildTwo( "embeddable_3" );
+			associationChildTwo.getManyToMany().add( session.find( AssociatedEntity.class, 3L ) );
+			associationChildTwo.getManyToMany().add( session.find( AssociatedEntity.class, 4L ) );
+			session.persist( new TestEntity( 3L, associationChildTwo ) );
+		} );
+		scope.inTransaction( session -> {
+			// queries
+			final List<AssociatedEntity> resultList = session.createQuery(
+					"select embeddable.manyToMany from TestEntity",
+					AssociatedEntity.class
+			).getResultList();
+			final Integer size = session.createQuery(
+					"select size(embeddable.manyToMany) from TestEntity where id = 3",
+					Integer.class
+			).getSingleResult();
+			assertThat( resultList ).hasSize( 2 ).hasSize( size )
+					.extracting( AssociatedEntity::getName )
+					.containsOnly( "associated_3", "associated_4" );
+		} );
+		scope.inTransaction( session -> {
+			// find
+			final TestEntity result = session.find( TestEntity.class, 3L );
+			assertThat( result.getEmbeddable().getParentProp() ).isEqualTo( "embeddable_3" );
+			assertThat( result.getEmbeddable() ).isExactlyInstanceOf( AssociationChildTwo.class );
+			final AssociationChildTwo embeddable = (AssociationChildTwo) result.getEmbeddable();
+			assertThat( embeddable.getManyToMany() ).hasSize( 2 )
+					.extracting( AssociatedEntity::getName )
+					.containsOnly( "associated_3", "associated_4" );
+			// update
+			embeddable.getManyToMany().remove( 1 );
+			final AssociatedEntity newAssociated = new AssociatedEntity( 44L, "associated_4_new" );
+			session.persist( newAssociated );
+			embeddable.getManyToMany().add( newAssociated );
+		} );
+		scope.inTransaction( session -> {
+			final TestEntity result = session.find( TestEntity.class, 3L );
+			final AssociationChildTwo embeddable = (AssociationChildTwo) result.getEmbeddable();
+			assertThat( embeddable.getManyToMany() ).hasSize( 2 )
+					.extracting( AssociatedEntity::getName )
+					.containsOnly( "associated_3", "associated_4_new" );
+		} );
+	}
+
+	@Test
+	public void testOneToMany(SessionFactoryScope scope) {
+		scope.inTransaction( session -> {
+			// insert
+			final AssociationChildThree associationChildTwo = new AssociationChildThree( "embeddable_3" );
+			associationChildTwo.getOneToMany().add( session.find( AssociatedEntity.class, 5L ) );
+			associationChildTwo.getOneToMany().add( session.find( AssociatedEntity.class, 6L ) );
+			session.persist( new TestEntity( 4L, associationChildTwo ) );
+		} );
+		scope.inTransaction( session -> {
+			// queries
+			final List<AssociatedEntity> resultList = session.createQuery(
+					"select embeddable.oneToMany from TestEntity",
+					AssociatedEntity.class
+			).getResultList();
+			final Integer size = session.createQuery(
+					"select size(embeddable.oneToMany) from TestEntity where id = 4",
+					Integer.class
+			).getSingleResult();
+			assertThat( resultList ).hasSize( 2 ).hasSize( size )
+					.extracting( AssociatedEntity::getName )
+					.containsOnly( "associated_5", "associated_6" );
+		} );
+		scope.inTransaction( session -> {
+			// find
+			final TestEntity result = session.find( TestEntity.class, 4L );
+			assertThat( result.getEmbeddable().getParentProp() ).isEqualTo( "embeddable_3" );
+			assertThat( result.getEmbeddable() ).isExactlyInstanceOf( AssociationChildThree.class );
+			final AssociationChildThree embeddable = (AssociationChildThree) result.getEmbeddable();
+			assertThat( embeddable.getOneToMany() ).hasSize( 2 )
+					.extracting( AssociatedEntity::getName )
+					.containsOnly( "associated_5", "associated_6" );
+			// update
+			embeddable.getOneToMany().remove( 0 );
+			final AssociatedEntity newAssociated = new AssociatedEntity( 55L, "associated_5_new" );
+			session.persist( newAssociated );
+			embeddable.getOneToMany().add( 0, newAssociated );
+		} );
+		scope.inTransaction( session -> {
+			final TestEntity result = session.find( TestEntity.class, 4L );
+			final AssociationChildThree embeddable = (AssociationChildThree) result.getEmbeddable();
+			assertThat( embeddable.getOneToMany() ).hasSize( 2 )
+					.extracting( AssociatedEntity::getName )
+					.containsOnly( "associated_5_new", "associated_6" );
+		} );
+	}
+
 	@BeforeAll
 	public void setUp(SessionFactoryScope scope) {
 		scope.inTransaction( session -> {
@@ -96,12 +200,17 @@ public class EmbeddableInheritanceAssciationsTest {
 			session.persist( new AssociatedEntity( 3L, "associated_3" ) );
 			session.persist( new AssociatedEntity( 4L, "associated_4" ) );
 			session.persist( new AssociatedEntity( 5L, "associated_5" ) );
+			session.persist( new AssociatedEntity( 6L, "associated_6" ) );
 		} );
 	}
 
 	@AfterAll
 	public void tearDown(SessionFactoryScope scope) {
 		scope.inTransaction( session -> {
+			session.createQuery( "from TestEntity where size(embeddable.oneToMany) > 0", TestEntity.class )
+					.getResultList()
+					.forEach( t -> ( (AssociationChildThree) t.getEmbeddable() ).getOneToMany().clear() );
+			session.flush();
 			session.createMutationQuery( "delete from TestEntity" ).executeUpdate();
 			session.createMutationQuery( "delete from AssociatedEntity" ).executeUpdate();
 		} );
@@ -211,7 +320,7 @@ public class EmbeddableInheritanceAssciationsTest {
 	@Embeddable
 	@DiscriminatorValue( "child_three" )
 	static class AssociationChildThree extends ParentEmbeddable {
-		@OneToMany
+		@OneToMany( fetch = FetchType.EAGER )
 		@JoinColumn
 		private List<AssociatedEntity> oneToMany = new ArrayList<>();
 
