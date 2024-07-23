434d98f3a2f662d31080d596fb5526ef764d0ffb
Marco Belladelli <marcobladel@gmail.com>
2024-07-23 15:50:19 +0200

    fixup! fixup! HHH-18391: Add failure test

--- hibernate-core/src/test/java/org/hibernate/orm/test/entitygraph/EntityGraphEmbeddedAttributesTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/entitygraph/EntityGraphEmbeddedAttributesTest.java
@@ -1,16 +1,17 @@
 package org.hibernate.orm.test.entitygraph;
 
 import java.util.Map;
-import java.util.UUID;
 
 import org.hibernate.Hibernate;
+import org.hibernate.Session;
 
+import org.hibernate.testing.orm.junit.DomainModel;
 import org.hibernate.testing.orm.junit.EntityManagerFactoryScope;
-import org.hibernate.testing.orm.junit.JiraKey;
-import org.hibernate.testing.orm.junit.Jpa;
+import org.hibernate.testing.orm.junit.Jira;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import jakarta.persistence.Embeddable;
@@ -22,60 +23,76 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hibernate.jpa.SpecHints.HINT_SPEC_FETCH_GRAPH;
 import static org.hibernate.jpa.SpecHints.HINT_SPEC_LOAD_GRAPH;
 
-@Jpa( annotatedClasses = {
+@DomainModel( annotatedClasses = {
 		EntityGraphEmbeddedAttributesTest.TrackedProduct.class,
 		EntityGraphEmbeddedAttributesTest.Tracking.class,
 		EntityGraphEmbeddedAttributesTest.UserForTracking.class,
 } )
-@JiraKey( "HHH-18391" )
+@SessionFactory
+@Jira( "https://hibernate.atlassian.net/browse/HHH-18391" )
 public class EntityGraphEmbeddedAttributesTest {
 	private static final int TRACKED_PRODUCT_ID = 1;
 
 	@Test
-	void testFetchGraph(EntityManagerFactoryScope scope) {
-		scope.inTransaction( entityManager -> {
-			final EntityGraph<TrackedProduct> trackedProductGraph = entityManager.createEntityGraph( TrackedProduct.class );
-			trackedProductGraph.addSubgraph( "tracking" ).addAttributeNodes( "creator" );
-			final TrackedProduct product = entityManager.find(
-					TrackedProduct.class,
-					TRACKED_PRODUCT_ID,
-					Map.of( "javax.persistence.fetchgraph", trackedProductGraph )
-			);
-			assertThat( Hibernate.isInitialized( product.tracking.creator ) ).isTrue();
-			assertThat( Hibernate.isInitialized( product.tracking.modifier ) ).isFalse();
-		} );
+	void testFetchGraphFind(SessionFactoryScope scope) {
+		executeTest( scope, HINT_SPEC_FETCH_GRAPH, true );
 	}
 
 	@Test
-	void testLoadGraph(EntityManagerFactoryScope scope) {
-		scope.inTransaction( entityManager -> {
-			final EntityGraph<TrackedProduct> trackedProductGraph = entityManager.createEntityGraph( TrackedProduct.class );
-			trackedProductGraph.addSubgraph( "tracking" ).addAttributeNodes( "creator" );
-			final TrackedProduct product = entityManager.createQuery(
+	void testFetchGraphQuery(SessionFactoryScope scope) {
+		executeTest( scope, HINT_SPEC_FETCH_GRAPH, false );
+	}
+
+	@Test
+	void testLoadGraphFind(SessionFactoryScope scope) {
+		executeTest( scope, HINT_SPEC_LOAD_GRAPH, true );
+	}
+
+	@Test
+	void testLoadGraphQuery(SessionFactoryScope scope) {
+		executeTest( scope, HINT_SPEC_LOAD_GRAPH, false );
+	}
+
+	static void executeTest(SessionFactoryScope scope, String hint, boolean find) {
+		scope.inTransaction( session -> {
+			final EntityGraph<TrackedProduct> graph = createGraph( session );
+			final TrackedProduct product = find ? session.find(
+					TrackedProduct.class,
+					TRACKED_PRODUCT_ID,
+					Map.of( hint, graph )
+			) : session.createQuery(
 					"from TrackedProduct",
 					TrackedProduct.class
-			).setHint( HINT_SPEC_LOAD_GRAPH, trackedProductGraph ).getSingleResult();
+			).setHint( hint, graph ).getSingleResult();
 			assertThat( Hibernate.isInitialized( product.tracking.creator ) ).isTrue();
 			assertThat( Hibernate.isInitialized( product.tracking.modifier ) ).isFalse();
 		} );
 	}
 
+	static EntityGraph<TrackedProduct> createGraph(Session session) {
+		final EntityGraph<TrackedProduct> graph = session.createEntityGraph( TrackedProduct.class );
+		graph.addSubgraph( "tracking" ).addAttributeNodes( "creator" );
+		return graph;
+	}
+
 	@BeforeAll
-	void setUp(EntityManagerFactoryScope scope) {
-		scope.inTransaction( entityManager -> {
+	void setUp(SessionFactoryScope scope) {
+		scope.inTransaction( session -> {
 			final UserForTracking creator = new UserForTracking( 1, "foo" );
-			entityManager.persist( creator );
-
+			session.persist( creator );
 			final UserForTracking modifier = new UserForTracking( 2, "bar" );
-			entityManager.persist( modifier );
-
-			final Tracking tracking = new Tracking( creator, modifier );
-
-			final TrackedProduct product = new TrackedProduct( TRACKED_PRODUCT_ID, UUID.randomUUID().toString(), tracking );
-			entityManager.persist( product );
+			session.persist( modifier );
+			final TrackedProduct product = new TrackedProduct( TRACKED_PRODUCT_ID, new Tracking( creator, modifier ) );
+			session.persist( product );
 		} );
+	}
+
+	@AfterAll
+	public void tearDown(SessionFactoryScope scope) {
+		scope.inTransaction( session -> session.getSessionFactory().getSchemaManager().truncateMappedObjects() );
 	}
 
 	@Entity( name = "TrackedProduct" )
@@ -86,14 +103,11 @@ public class EntityGraphEmbeddedAttributesTest {
 		@Embedded
 		private Tracking tracking;
 
-		private String barcode;
-
 		public TrackedProduct() {
 		}
 
-		public TrackedProduct(Integer id, String barcode, Tracking tracking) {
+		public TrackedProduct(Integer id, Tracking tracking) {
 			this.id = id;
-			this.barcode = barcode;
 			this.tracking = tracking;
 		}
 	}
